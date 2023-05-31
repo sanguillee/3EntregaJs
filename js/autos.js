da19d02d36b594088a48dcba4108438c93a0cdaf
santiago Guilleminot <sansound_dj@hotmail.com>
2023-05-30 23:45:26 -0300

    grabo 2da pre entrega

---
+++ js/autos.js
@@ -0,0 +1,28 @@
+let stockProductos= [
+    
+       { id: 1,
+        nombre: "Chevrolet Camaro",
+        caracteristicas: "(Motor 6.2 v8, 405 cv, convertible, automatico)",
+        precio:  '110.000',
+        img: './img/camaro.jpg',} ,
+   
+        { id: 2,
+        nombre: "Chevrolet corvette",
+        caracteristicas: "(Motor 7.0 v8, 495cv, automatico)",
+        precio: '200.000',
+        img: './img/corvette.jpg',},
+
+      { id: 4,
+        nombre: "Audi R8",
+        caracteristicas: "(Motor 5.2 v10, 610cv, automatico)",
+        precio: '400.000',
+        img: './img/r8.jpg',},
+        
+      { id: 3,
+        nombre: "Ford Mustang",
+        caracteristicas: "(Motor 5.0 v8, 421cv, automatico)",
+        precio: '150.000',
+        img: './img/mustang.jpg',},
+      ];
+    
+
